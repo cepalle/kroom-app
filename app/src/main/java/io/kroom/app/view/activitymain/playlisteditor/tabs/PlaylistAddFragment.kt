cc2584f032251fa104b5fb1ae8744453757ca443
cepalle <epalle.cyrille@gmail.com>
2019-06-24 11:19:45 +0200

    PlaylistAddViewModel

--- app/src/main/java/io/kroom/app/view/activitymain/playlisteditor/tabs/PlaylistAddFragment.kt
+++ app/src/main/java/io/kroom/app/view/activitymain/playlisteditor/tabs/PlaylistAddFragment.kt
@@ -5,10 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import io.kroom.app.R
 
 class PlaylistAddFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_playlist_editor_tab_add, container, false)
     }
+
+    override fun onStart() {
+        super.onStart()
+
+        val model = ViewModelProviders.of(this).get(PlaylistPublicViewModel::class.java)
+    }
+
 }
---
+++ app/src/main/java/io/kroom/app/view/activitymain/playlisteditor/tabs/PlaylistAddViewModel.kt
@@ -0,0 +1,16 @@
+package io.kroom.app.view.activitymain.playlisteditor.tabs
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import io.kroom.app.repo.PlaylistEditorRepo
+import io.kroom.app.repo.webservice.GraphClient
+import io.kroom.app.util.SharedPreferences
+
+class PlaylistAddViewModel(app: Application) : AndroidViewModel(app) {
+    private val client = GraphClient {
+        SharedPreferences.getToken(getApplication())
+    }.client
+
+    private val playRepo = PlaylistEditorRepo(client)
+
+}
