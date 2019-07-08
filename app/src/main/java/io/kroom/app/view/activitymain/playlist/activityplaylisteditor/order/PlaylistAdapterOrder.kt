0af9d08eec848ca58585b1f34932c0ff323dcc7d
epalle <epalle.cyrille@gmail.com>
2019-07-08 19:31:47 +0200

    playlistOrder, fragment viewModel adapter

---
+++ app/src/main/java/io/kroom/app/view/activitymain/playlist/activityplaylisteditor/order/PlaylistAdapterOrder.kt
@@ -0,0 +1,85 @@
+package io.kroom.app.view.activitymain.playlist.activityplaylisteditor.order
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import io.kroom.app.R
+
+
+data class playAdapterOrderModel(
+    val id: Int,
+    val name: String,
+    val userName: String,
+    val nbTrack: Int,
+    val public: Boolean
+)
+
+const val EXTRA_NAME_PLAYLIST_ID = "PlaylistPublicAdapter.playlistId"
+
+class PlaylistAdapterOrder(private val dataSet: MutableList<playAdapterOrderModel>, mContext: Context) :
+    ArrayAdapter<playAdapterOrderModel>(mContext, R.layout.adapter_playlist_public, dataSet) {
+
+    fun updateDataSet(todos: List<playAdapterOrderModel>) {
+        dataSet.clear()
+        dataSet.addAll(todos)
+    }
+
+    private class ViewHolder {
+        var cacheName: TextView? = null
+        var cacheUserName: TextView? = null
+        var cacheNbTrack: TextView? = null
+        var cachePrivacy: TextView? = null
+        var cacheButtonReading: Button? = null
+        var cacheButtonEdition: Button? = null
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var convertView = convertView
+        val dataModel = dataSet[position]
+        val viewHolder: ViewHolder // view lookup cache stored in tag
+
+        if (convertView == null) {
+            viewHolder = ViewHolder()
+            val inflater = LayoutInflater.from(context)
+            convertView = inflater.inflate(R.layout.adapter_playlist_public, parent, false)
+            viewHolder.cacheName = convertView.findViewById(R.id.adapter_name)
+            viewHolder.cacheNbTrack = convertView.findViewById(R.id.adapter_nb_track)
+            viewHolder.cachePrivacy = convertView.findViewById(R.id.adapter_privacy)
+            viewHolder.cacheUserName = convertView.findViewById(R.id.adapter_user_name)
+            viewHolder.cacheButtonReading = convertView.findViewById(R.id.adapter_button_reading)
+            viewHolder.cacheButtonEdition = convertView.findViewById(R.id.adapter_button_edition)
+
+            convertView.tag = viewHolder
+        } else {
+            viewHolder = convertView.tag as ViewHolder
+        }
+
+        viewHolder.cacheName?.text = dataModel.name
+        viewHolder.cacheUserName?.text = "by ${dataModel.userName}"
+        viewHolder.cacheNbTrack?.text = "${dataModel.nbTrack} Tracks"
+        viewHolder.cachePrivacy?.text = if (dataModel.public) "public" else "private"
+        viewHolder.cacheButtonEdition?.setOnClickListener {
+            val intent = Intent(context, PlaylistEditorActivity::class.java).apply {
+                putExtra(EXTRA_NAME_PLAYLIST_ID, dataModel.id)
+            }
+            ContextCompat.startActivity(context, intent, null)
+        }
+        viewHolder.cacheButtonReading?.setOnClickListener {
+            val intent = Intent(context, PlaylistReadingActivity::class.java).apply {
+                putExtra(EXTRA_NAME_PLAYLIST_ID, dataModel.id)
+            }
+            ContextCompat.startActivity(context, intent, null)
+        }
+
+        // convertView?.setBackgroundColor(dataModel.color.toColor())
+        return convertView!!
+    }
+}
--- app/src/main/java/io/kroom/app/view/activitymain/playlist/activityplaylisteditor/order/PlaylistEditorOrderFragement.kt
+++ app/src/main/java/io/kroom/app/view/activitymain/playlist/activityplaylisteditor/order/PlaylistEditorOrderFragement.kt
@@ -1,21 +1,37 @@
 package io.kroom.app.view.activitymain.playlist.activityplaylisteditor.order
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import io.kroom.app.R
+import kotlinx.android.synthetic.main.fragment_playlist_editor_tab_order.*
 
 
 class PlaylistEditorOrderFragement(val playlistId: Int) : Fragment() {
+
+    private lateinit var adapterAutocompletion: ArrayAdapter<String>
+    private val adapterTracks by lazy {
+        context?.let {
+            PlaylistAdapterOrder(arrayListOf(), it)
+        }
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_playlist_editor_tab_order, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        adapterAutocompletion = ArrayAdapter(activity!!, R.layout.select_dialog_item_material, java.util.ArrayList())
+        playlistEditorTabOrderAutoCompleteTextView.setAdapter(adapterAutocompletion)
 
         val model = ViewModelProviders.of(
             this,
@@ -25,6 +41,45 @@ class PlaylistEditorOrderFragement(val playlistId: Int) : Fragment() {
         val tracksList = model.getTracksList()
         val errorMessage = model.getErrorMessage()
 
+        playlistEditorTabOrderAutoCompleteTextView.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(
+                s: CharSequence, start: Int,
+                before: Int, count: Int
+            ) {
+                model.updateAutoComplet(playlistEditorTabOrderAutoCompleteTextView.text.toString())
+            }
+        })
+
+        updateAutoCompletion(autoCompletion.value)
+        autoCompletion.observe(this, Observer {
+            updateAutoCompletion(it)
+        })
+
+        errorMessage.observe(this, Observer {
+            playlistEditorTabOrderAutoCompleteTextView.error = it
+        })
+
+
+        updateListTracks(tracksList.value)
+        tracksList.observe(this, Observer {
+            updateListTracks(it)
+        })
+    }
+
+    private fun updateAutoCompletion(ls: List<Pair<String, Int>>?) {
+        ls ?: return
+        adapterAutocompletion.clear()
+        adapterAutocompletion.addAll(ls.map { it.first })
+        adapterAutocompletion.notifyDataSetChanged()
+    }
+
+    private fun updateListTracks(ls: List<Pair<String, Int>>?) {
+        ls ?: return
+        adapterTracks?.updateDataSet(ls)
+        adapterTracks?.notifyDataSetChanged()
     }
 
 }
