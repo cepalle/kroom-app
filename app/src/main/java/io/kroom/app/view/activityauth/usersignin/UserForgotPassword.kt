f46d25da93fd2ecab62beab170a6642eb53e63f2
cepalle <epalle.cyrille@gmail.com>
2019-08-21 12:25:15 +0200

    sav

--- app/src/main/java/io/kroom/app/view/activityauth/usersignin/UserForgotPassword.kt
+++ app/src/main/java/io/kroom/app/view/activityauth/usersignin/UserForgotPassword.kt
@@ -1,13 +1,42 @@
 package io.kroom.app.view.activityauth.usersignin
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import io.kroom.app.repo.UserRepo
+import io.kroom.app.util.Session
+import io.kroom.app.webservice.GraphClient
+import kotlinx.android.synthetic.main.activity_forgot_password.*
 
 class UserForgotPassword : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         title = "Forgot password"
         setContentView(io.kroom.app.R.layout.activity_forgot_password)
+
+
+        forgotPasswordSubmit.setOnClickListener {
+            val client = GraphClient {
+                Session.getToken(application)
+            }.client
+
+            val userRepo = UserRepo(client)
+
+            if (forgotPasswordPassword.text.isNotBlank() && forgotPasswordEmail.text.isNotBlank()) {
+                userRepo.updatePassword(
+                    forgotPasswordPassword.text.toString(),
+                    forgotPasswordEmail.text.toString()
+                ).observe(this, Observer {
+                    it.onFailure {
+                        Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show()
+                    }
+                    it.onSuccess {
+                        finish()
+                    }
+                })
+            }
+        }
 
     }
 }
