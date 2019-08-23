ef82a7f1b394ef799174b859c02ac05ae3de6cce
cepalle <epalle.cyrille@gmail.com>
2019-08-23 11:08:21 +0200

    up schema

--- app/src/main/java/io/kroom/app/view/activitymain/trackvoteevent/activitymusictrackvote/MusicTrackVoteEditorFragement.kt
+++ app/src/main/java/io/kroom/app/view/activitymain/trackvoteevent/activitymusictrackvote/MusicTrackVoteEditorFragement.kt
@@ -5,24 +5,43 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import io.kroom.app.R
 import kotlinx.android.synthetic.main.fragment_music_track_vote_editor.*
 
 class MusicTrackVoteEditorFragement(private val eventId: Int) : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_music_track_vote_editor, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val model = activity?.let {
-            ViewModelProviders.of(it).get(MusicTrackVoteEditorViewModel::class.java)
-        }
+        val model = ViewModelProviders.of(
+            this,
+            MusicTrackVoteEditorViewModelFactory(this.activity!!.application, eventId)
+        ).get(MusicTrackVoteEditorViewModel::class.java)
 
-        model ?: return
+        model.getErrorMsg().observe(this, Observer {
+            Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
+        })
+
+        model.getInputMusicEditor().observe(this, Observer {
+            musicTrackVoteEventEditorInputName.setText(it.name)
+            musicTrackVoteEditorInputBegin.setText(it.begin)
+            musicTrackVoteEditorInputEnd.setText(it.end)
+            musicTrackVoteEditorInputLongitude.setText(it.longitude.toString())
+            musicTrackvoteEditorInputLatitude.setText(it.latitude.toString())
+            musicTrackVoteEditorPublicSwitch.isChecked = it.public
+            musicTrackVoteEditorLocAndSchRestrictionSwitch.isChecked = it.locAndSchRestriction
+        })
 
         musicTrackVoteEventEditorButtonDel.setOnClickListener {
             model.delTrackVote(eventId)
--- app/src/main/java/io/kroom/app/view/activitymain/trackvoteevent/activitymusictrackvote/MusicTrackVoteEditorViewModel.kt
+++ app/src/main/java/io/kroom/app/view/activitymain/trackvoteevent/activitymusictrackvote/MusicTrackVoteEditorViewModel.kt
@@ -1,25 +1,83 @@
 package io.kroom.app.view.activitymain.trackvoteevent.activitymusictrackvote
 
 import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
+import androidx.lifecycle.*
 import io.kroom.app.graphql.TrackVoteEventDelMutation
 import io.kroom.app.repo.TrackVoteEventRepo
 import io.kroom.app.util.Session
 import io.kroom.app.webservice.GraphClient
 
-class MusicTrackVoteEditorViewModel(app: Application) : AndroidViewModel(app) {
+class MusicTrackVoteEditorViewModelFactory(
+    private val mApplication: Application,
+    private val mParam: Int
+) :
+    ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return MusicTrackVoteEditorViewModel(mApplication, mParam) as T
+    }
+}
+
+data class InputMusicEditor(
+    val name: String,
+    val begin: String,
+    val end: String,
+    val latitude: Double,
+    val longitude: Double,
+    val public: Boolean,
+    val locAndSchRestriction: Boolean
+)
+
+class MusicTrackVoteEditorViewModel(app: Application, private val eventId: Int) :
+    AndroidViewModel(app) {
     private val client = GraphClient {
         Session.getToken(getApplication())
     }.client
 
     private val repoTrack = TrackVoteEventRepo(client)
 
-    // fetch on init
-    // create model
+    private val inputMusicEditor: MediatorLiveData<InputMusicEditor> = MediatorLiveData()
+    private val errorMessage: MediatorLiveData<String> = MediatorLiveData()
+
+    init {
+        inputMusicEditor.addSource(repoTrack.getTrackVoteEventById(eventId)) {
+            it.onFailure {
+                errorMessage.postValue(it.message)
+                inputMusicEditor.postValue(null)
+            }
+            it.onSuccess {
+
+                if (it.TrackVoteEventById().errors().isNotEmpty()) {
+                    errorMessage.postValue(it.TrackVoteEventById().errors()[0].messages()[0])
+                } else {
+                    val info = it.TrackVoteEventById().trackVoteEvent()
+                    info ?: return@addSource
+                    inputMusicEditor.postValue(
+                        InputMusicEditor(
+                            info.name(),
+                            info.scheduleBegin() ?: return@addSource,
+                            info.scheduleEnd() ?: return@addSource,
+                            info.latitude() ?: return@addSource,
+                            info.longitude() ?: return@addSource,
+                            info.public_(),
+                            info.locAndSchRestriction()
+                        )
+                    )
+                }
+            }
+        }
+    }
 
     fun delTrackVote(eventId: Int): LiveData<Result<TrackVoteEventDelMutation.Data>> {
         return repoTrack.del(eventId)
+    }
+
+    fun getErrorMsg(): LiveData<String> {
+        return errorMessage
+    }
+
+    fun getInputMusicEditor(): LiveData<InputMusicEditor> {
+        return inputMusicEditor
     }
 
 }
