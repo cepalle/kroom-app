b29b4b86264381657eaf497d27296d33b8e2f050
helene <helene?chretien01@gmail.com>
2019-08-27 12:13:54 +0200

    add trackvote in progress

--- app/src/main/java/io/kroom/app/view/activitymain/trackvoteevent/activitymusictrackvote/MusciTrackVoteSearchAddTrackActivity.kt
+++ app/src/main/java/io/kroom/app/view/activitymain/trackvoteevent/activitymusictrackvote/MusciTrackVoteSearchAddTrackActivity.kt
@@ -7,11 +7,13 @@ import io.kroom.app.R
 
 class MusciTrackVoteSearchAddTrackActivity : AppCompatActivity()  {
 
+    var eventId: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         title = "Search & Add Track"
         setContentView(R.layout.activity_music_track_vote_search_add)
+        eventId = intent.getIntExtra(EXTRA_EVENT_ID, 0)
 
     }
 }
